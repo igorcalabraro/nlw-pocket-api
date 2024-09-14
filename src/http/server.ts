ddfe78238723ca413a9518e2ddd03c19aefdb2b3
Igor <igor.calabraro@aluno.viscondeitaborai.com.br>
2024-09-14 14:19:10 -0300

    feat: get week summary

--- src/http/server.ts
+++ src/http/server.ts
@@ -7,6 +7,7 @@ import {
 import { createGoalRoute } from './routes/create-goal'
 import { createCompletionRoute } from './routes/create-completion'
 import { getPendingGoalsRoute } from './routes/get-pending-goals'
+import { getWeekSummaryRoute } from './routes/get-week-summary'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 app.setValidatorCompiler(validatorCompiler)
@@ -15,6 +16,7 @@ app.setSerializerCompiler(serializerCompiler)
 app.register(createGoalRoute)
 app.register(createCompletionRoute)
 app.register(getPendingGoalsRoute)
+app.register(getWeekSummaryRoute)
 
 app
   .listen({
